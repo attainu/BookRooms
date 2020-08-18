1a4829e351048317201acdc20510954cde7cfac2
Tarun Das <imtarun79@gmail.com>
2020-08-18 21:11:35 +0530

    final code updated

--- Project/bookrooms/src/pages/ContactUs.js
+++ Project/bookrooms/src/pages/ContactUs.js
@@ -1,68 +1,37 @@
 import React from "react";
 import "../styles/contact.css";
-import { FaMapMarkerAlt, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+
 const ContactUs = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
   };
-
   return (
     <div className="section">
       <div className="container">
-        <div className="contactinfo">
-          <div>
-            <h2>Contact Info</h2>
-            <ul className="info">
-              <li>
-                <p>
-                  <FaMapMarkerAlt /> 2912 WhiteField
-                  <br />
-                  Bangalore <br />
-                  200042
-                </p>
-              </li>
-              <li>
-                <span>
-                  <FaEnvelope /> Bookrooms@gmail.com
-                </span>
-              </li>
-              <li>
-                <span>
-                  {" "}
-                  <FaMobileAlt /> 310-357-1334
-                </span>
-              </li>
-            </ul>
-            <ul className="sci"></ul>
+        <div className="contact-box">
+          <div className="left"></div>
+          <div className="right">
+            <form>
+              <h2>Contact Us</h2>
+              <input
+                type="text"
+                className="field"
+                placeholder="Your Name"
+                name=""
+                required
+              />
+              <input type="text" className="field" placeholder="Your Email" />
+              <input type="text" className="field" placeholder="Phone" />
+              <textarea placeholder="Message" className="field"></textarea>
+              <button
+                className="btn"
+                onSubmit={handleSubmit}
+                style={{ textAlign: "center" }}
+              >
+                Send
+              </button>
+            </form>
           </div>
-        </div>
-        <div className="contactForm">
-          <h2>Send a message</h2>
-          <form className="formBox">
-            <div class="inputBox w50">
-              <input type="text" name="" required />
-              <span>First Name</span>
-            </div>
-            <div class="inputBox w50">
-              <input type="text" name="" required />
-              <span>Last Name</span>
-            </div>
-            <div class="inputBox w50">
-              <input type="email" name="" required />
-              <span>Email Address</span>
-            </div>
-            <div class="inputBox w50">
-              <input type="tel" name="" required />
-              <span>Mobile Number</span>
-            </div>
-            <div class="inputBox w100">
-              <textarea name="" required></textarea>
-              <span>Write your message here</span>
-            </div>
-            <div class="inputBox w100">
-              <input type="submit" value="Send" onSubmit={handleSubmit} />
-            </div>
-          </form>
         </div>
       </div>
     </div>
